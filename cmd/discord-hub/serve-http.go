b296d5c85e000756de488a16ced0be2bd390215c
Maartje Eyskens <maartje@eyskens.me>
2021-09-21 20:41:59 +0200

    Actally do something with waitlist calls

--- cmd/discord-hub/serve-http.go
+++ cmd/discord-hub/serve-http.go
@@ -6,6 +6,7 @@ import (
 	"sort"
 
 	"github.com/bwmarrin/discordgo"
+	"github.com/itfactory-tm/thomas-bot/pkg/embed"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 	"github.com/thomasmorebe/discord-hub/pkg/data"
@@ -14,6 +15,8 @@ import (
 
 	"github.com/spf13/cobra"
 )
+
+const emailChannel = "889939643506786334"
 
 func init() {
 	rootCmd.AddCommand(NewServeHTTPCmd())
@@ -73,11 +76,51 @@ func (s *serveHTTPCmdOptions) RunE(cmd *cobra.Command, args []string) error {
 	e.Static("/", "./www")
 
 	e.POST("/api/servers/:guild/join", s.handleInvite)
+	e.POST("/api/waitlist", s.handleWaitlist)
 	e.GET("/api/servers", s.handleGetServers)
 	e.GET("/api/tags", s.handleGetTags)
 
 	// start HTTP server
 	return e.Start(s.BindAddr)
+}
+
+func (s *serveHTTPCmdOptions) handleWaitlist(c echo.Context) error {
+	ip := c.Request().Header.Get("CF-Connecting-IP")
+	if ip == "" {
+		return c.JSON(http.StatusBadRequest, echo.Map{"error": "not proxied correct"})
+	}
+
+	data := struct {
+		Email        string `json:"email"`
+		Campus       string `json:"campus"`
+		Programme    string `json:"programme"`
+		HcapthaToken string `json:"h-captcha-response"`
+	}{}
+	c.Bind(&data)
+
+	if data.HcapthaToken == "" {
+		return c.JSON(http.StatusBadRequest, echo.Map{"error": "h-captcha-response is required"})
+	}
+
+	if !s.verifyCaptcha(ip, data.HcapthaToken) {
+		return c.JSON(http.StatusBadRequest, echo.Map{"error": "h-captcha-response is invalid"})
+	}
+
+	e := embed.NewEmbed()
+	e.SetTitle("new signup for waitlist")
+	e.AddField("email", data.Email)
+	e.AddField("campus", data.Campus)
+	e.AddField("programme", data.Programme)
+
+	dg, err := discordgo.New("Bot " + s.Token)
+	if err != nil {
+		log.Println(err)
+		return c.JSON(http.StatusInternalServerError, echo.Map{"error": "Cannot connect to Discord"})
+	}
+
+	dg.ChannelMessageSendEmbed(emailChannel, e.MessageEmbed)
+
+	return c.JSON(http.StatusOK, echo.Map{"status": "ok"})
 }
 
 func (s *serveHTTPCmdOptions) handleInvite(c echo.Context) error {
--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.14
 require (
 	github.com/bwmarrin/discordgo v0.23.2
 	github.com/golang/glog v0.0.0-20160126235308-23def4e6c14b
+	github.com/itfactory-tm/thomas-bot v0.0.0-20210920084722-6c499270958c // indirect
 	github.com/labstack/echo/v4 v4.5.0
 	github.com/meyskens/go-hcaptcha v0.0.0-20200428113538-5c28ead635cd
 	github.com/spf13/cobra v1.1.3
