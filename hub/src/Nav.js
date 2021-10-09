a881a1a83e53c8738a21ad497c65d0ea563ae8e2
Niels Peeters <nielspeeters11@hotmail.com>
2021-10-09 22:27:59 +0200

    Align nav style to that one of thomasmore.be

--- hub/src/Nav.js
+++ hub/src/Nav.js
@@ -1,4 +1,4 @@
-import { Navbar, NavDropdown, NavItem, NavLink, Container } from "react-bootstrap";
+import { Navbar, NavDropdown, NavItem, NavLink } from "react-bootstrap";
 import i18n from "i18next";
 import { useTranslation } from "react-i18next";
 
@@ -6,27 +6,26 @@ function AppNav() {
   const { t } = useTranslation();
 
   return (
-    <Navbar bg="white" expand="lg">
-      <Container expand="lg">
-        <Navbar.Brand href="#home">
-          <img src="/logo.svg" alt="Thomas More" height="53" />
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end bg-white">
-          <NavItem className="normal-link">
-            <NavLink href="https://thomasmore.be">Home</NavLink>
-          </NavItem>
-          <NavItem className="normal-link">
-            <NavLink href="https://thomasmore.be">Opleidingen</NavLink>
-          </NavItem>
-          <NavDropdown title={t("select_language")}>
-            <NavDropdown.Item onClick={() => i18n.changeLanguage("nl")}>NL</NavDropdown.Item>
-            <NavDropdown.Item onClick={() => i18n.changeLanguage("EN")}>EN</NavDropdown.Item>
-          </NavDropdown>
-        </Navbar.Collapse>
-      </Container>
-
-    </Navbar>
+    <header className="site-header">
+      <Navbar bg="white" expand="lg">
+          <Navbar.Brand href="#home">
+            <img src="/logo.svg" alt="Thomas More" height="53" />
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end bg-white">
+            <NavItem className="normal-link">
+              <NavLink href="https://thomasmore.be">Home</NavLink>
+            </NavItem>
+            <NavItem className="normal-link">
+              <NavLink href="https://thomasmore.be">Opleidingen</NavLink>
+            </NavItem>
+            <NavDropdown title={t("select_language")}>
+              <NavDropdown.Item onClick={() => i18n.changeLanguage("nl")}>NL</NavDropdown.Item>
+              <NavDropdown.Item onClick={() => i18n.changeLanguage("EN")}>EN</NavDropdown.Item>
+            </NavDropdown>
+          </Navbar.Collapse>
+      </Navbar>
+    </header>
   );
 }
 
